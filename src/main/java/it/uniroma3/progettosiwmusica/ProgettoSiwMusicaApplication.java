82b875c6471571d1198833f168fb97893ff43b05
gabgalletti <gab.galletti@stud.uniroma3.it>
2025-05-15 11:09:26 +0200

    inserimento file iniziali

---
+++ src/main/java/it/uniroma3/progettosiwmusica/ProgettoSiwMusicaApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.progettosiwmusica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSiwMusicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgettoSiwMusicaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/uniroma3/progettosiwmusica/ProgettoSiwMusicaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.progettosiwmusica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSiwMusicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
