e0a6dc1e540544dddaf30299992503a1639bc841
gabgalletti <gab.galletti@stud.uniroma3.it>
2025-05-20 09:31:57 +0200

    modifica entità musica

--- src/main/java/it/uniroma3/progettosiwmusica/model/Music.java
+++ src/main/java/it/uniroma3/progettosiwmusica/model/Music.java
@@ -12,4 +12,20 @@ public class Music {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
