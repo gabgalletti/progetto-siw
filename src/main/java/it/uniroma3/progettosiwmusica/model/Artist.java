a8871bbfe49811ac43dee2e535298e6ea2a21c71
valvirdia <val.virdia1@stud.uniroma3.it>
2025-06-18 14:42:04 +0200

    modifica home,music/s,artist/s,form. modifica relativi file css, aggiunta profilo

--- src/main/java/it/uniroma3/progettosiwmusica/model/Artist.java
+++ src/main/java/it/uniroma3/progettosiwmusica/model/Artist.java
@@ -29,13 +29,6 @@ public class Artist {
     @OneToMany(mappedBy = "artist", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Music> musics = new ArrayList<>();
 
-    private String imageUrl;
-
-    public String getImageUrl() {return imageUrl;}
-
-    public void setImageUrl(String imageUrl) {this.imageUrl = imageUrl;}
-
-
     public Long getId() {
         return id;
     }
